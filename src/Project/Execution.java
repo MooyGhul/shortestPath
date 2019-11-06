24788e3e672e3e5bf44e53d0da5869bbbaccf407
panwangp <panwangp@sfu.ca>
2019-11-06 07:48:57 -0800

    add .gitigonore

--- src/Project/Execution.java
+++ src/Project/Execution.java
@@ -5,36 +5,29 @@ import java.net.URL;
 
 public class Execution {
 
-
-
     public static void main(String[] args){
-
-
-
 
         try {
             URL urlfile = new URL("https://www.cs.sfu.ca/~binay/2019/cmpt307/graph1000.txt");
             Graph graph = new Graph(urlfile);
 
-//            System.out.println("---------------- DIJKSTRA ----------------");
-//            Dijkastra dijkastra= new Dijkastra();
+            /**System.out.println("----------------PRINT DIJKSTRA PATH----------------");*/
+            // Dijkastra dijkastra= new Dijkastra();
+            // double shortestPath = dijkastra.visit(graph,source,target);
+            // System.out.println("Distance = "+ Tool.df2.format(shortestPath));
 
-//            double shortestPath = dijkastra.visit(graph,source,target);
-//            System.out.println("Distance = "+ Tool.df2.format(shortestPath));
-
-//            System.out.println("------------------- A* -------------------");
-
-//            Astar astar = new Astar();
-//            System.out.println("Distance = "+ Tool.df2.format(astar.visit(graph,source,target)));
+            /**System.out.println("-------------------PRINT A* PATH-------------------");*/
+            // Astar astar = new Astar();
+            // System.out.println("Distance = "+ Tool.df2.format(astar.visit(graph,source,target)));
 
 
-//            System.out.println("--------------- A* landmark ----------------");
+            /**System.out.println("---------------PRINT A* landmark PATH----------------");*/
+            // AStarWithLandmark alandmark = new AStarWithLandmark();
+            // System.out.println("Distance = "+ Tool.df2.format(alandmark.visit(graph,source,target)));
 
-//            AStarWithLandmark alandmark = new AStarWithLandmark();
-//            System.out.println("Distance = "+ Tool.df2.format(alandmark.visit(graph,source,target)));
 
-
-            for(int i = 1; i<=10;i++){
+            /**generate 20 random test cases*/
+            for(int i = 1; i<=20;i++){
 
                 Graph_Node source = graph.getNodes()[(int)(Math.random()*1000+1)];
                 Graph_Node target = graph.getNodes()[(int)(Math.random()*1000+1)];
@@ -47,19 +40,15 @@ public class Execution {
                 AStarWithLandmark alandmark = new AStarWithLandmark();
                 double l_shortestPath = alandmark.visit(graph,source,target);
 
-                System.out.println("Round "+ i +" is "+ ((d_shortestPath==a_shortestPath)==(a_shortestPath==l_shortestPath)) );
 
+                //System.out.println("Round "+ i +" is "+ ((d_shortestPath==a_shortestPath)==(a_shortestPath==l_shortestPath)) );
                 System.out.println("Dijkastra visited: "+dijkastra.visitedNodeCounter + " , Astar visited: "+astar.visitedNodeCounter +" , LandMark Visited "+alandmark.visitedNodeCounter);
-                // why i cannot push it to github
 
             }
-
 
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
-
-
     }
 
 
--- src/Project/Pair.java
+++ src/Project/Pair.java
@@ -21,9 +21,6 @@ public class Pair<T, U> {
         this.second = distance;
     }
 
-    // Because 'pair()' is shorter than 'new Pair<>()'.
-    // Sometimes this difference might be very significant (especially in a
-    // 80-ish characters boundary). Sorry diamond operator.
     public static <T, U> Pair<T, U> pair(T first, U second) {
         return new Pair<>(first, second);
     }
